b1fd2ff609708eb1b656a8d69c65abcd217b8620
solom.wm <solom.on.wm00017@gmail.com>
2018-09-04 00:26:32 +0300

    Start Project

---
+++ TurretWebService/Data/TurretWebServiceContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TurretWebService.Models
+{
+    public class TurretWebServiceContext : DbContext
+    {
+        public TurretWebServiceContext (DbContextOptions<TurretWebServiceContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TurretWebService.Models.User> User { get; set; }
+    }
+}
